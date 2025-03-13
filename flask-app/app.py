432a00508717b6865db92a4f7b258db998182185
Cacy45 <167324266+Cacy45@users.noreply.github.com>
2025-03-13 10:32:56 +0200

    commit for p review

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/student_login')
+def student_login():
+    return render_template('student_login.html')
+
+@app.route('/student_dashboard')
+def student_dashboard():
+    return render_template('student_dashboard.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
